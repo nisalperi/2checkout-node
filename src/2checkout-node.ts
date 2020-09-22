e11037d0dba46c93985d0e6b4c77c8e55342e484
Nisal Periyapperuma <nisalp2006@gmail.com>
2020-09-22 23:29:31 +0530

    Fix dist bug

--- src/2checkout-node.ts
+++ src/2checkout-node.ts
@@ -1,83 +1,89 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-import { createHmac } from 'crypto';
-import * as moment from 'moment';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import { createHmac } from 'crypto'
+import * as moment_ from 'moment'
 
-const defaultHost = 'https://api.2checkout.com/rest/6.0';
-const defaultSandboxHost = 'https://api.2checkout.com/rest/6.0';
+const moment = moment_
 
-interface TwoCheckoutClientInitParams{
-  merchantCode: string;
-  secretKey: string;
-  sandbox?: boolean;
+const defaultHost = 'https://api.2checkout.com/rest/6.0'
+const defaultSandboxHost = 'https://api.2checkout.com/rest/6.0'
+
+interface TwoCheckoutClientInitParams {
+  merchantCode: string
+  secretKey: string
+  sandbox?: boolean
 }
 
 const generateHash = (val: string, salt: string) => {
-  return createHmac('md5', salt).update(val).digest('hex');
+  return createHmac('md5', salt)
+    .update(val)
+    .digest('hex')
 }
 
 export default class TwoCheckoutClient {
-  merchantCode: string;
-  secretKey: string;
-  hostURL: string;
+  merchantCode: string
+  secretKey: string
+  hostURL: string
 
-  constructor(params: TwoCheckoutClientInitParams){
-    this.merchantCode = params.merchantCode;
-    this.secretKey = params.secretKey;
-    this.hostURL = defaultHost;
+  constructor(params: TwoCheckoutClientInitParams) {
+    this.merchantCode = params.merchantCode
+    this.secretKey = params.secretKey
+    this.hostURL = defaultHost
 
     if (params.sandbox) {
-      this.hostURL = defaultSandboxHost;
+      this.hostURL = defaultSandboxHost
     }
   }
 
   private hashValue(rawString: string): string {
-    return generateHash(rawString, this.secretKey);
+    return generateHash(rawString, this.secretKey)
   }
 
   private getAuthenticationHeaderValue() {
-    let now = moment().utc().format('YYYY-MM-DD HH:mm:ss');
-    let toHash = this.merchantCode.length + this.merchantCode + now.length + now;
-    return `code="${this.merchantCode}" date="${now}" hash="${this.hashValue(toHash)}"`;
+    let now = moment()
+      .utc()
+      .format('YYYY-MM-DD HH:mm:ss')
+    let toHash = this.merchantCode.length + this.merchantCode + now.length + now
+    return `code="${this.merchantCode}" date="${now}" hash="${this.hashValue(toHash)}"`
   }
 
   static serializeArray(obj: Object): string {
-    let tmp = '';
+    let tmp = ''
     for (const [key, value] of Object.entries(obj)) {
-      if (key === 'HASH') continue;
+      if (key === 'HASH') continue
       if (Array.isArray(value)) {
-        tmp += this.serializeArray(value);
+        tmp += this.serializeArray(value)
       } else {
-        tmp += value.length + value;
+        tmp += value.length + value
       }
     }
-    return tmp;
+    return tmp
   }
 
-  getLCNHash(obj: Object): string{
-    return this.hashValue(TwoCheckoutClient.serializeArray(obj));
+  getLCNHash(obj: Object): string {
+    return this.hashValue(TwoCheckoutClient.serializeArray(obj))
   }
 
   getIPNHash(obj: Object): string {
-    return this.hashValue(TwoCheckoutClient.serializeArray(obj));
+    return this.hashValue(TwoCheckoutClient.serializeArray(obj))
   }
 
   private getInstance(): AxiosInstance {
-    const token = this.getAuthenticationHeaderValue();
+    const token = this.getAuthenticationHeaderValue()
     return axios.create({
       baseURL: this.hostURL,
       headers: {
-        'X-Avangate-Authentication': token,
+        'X-Avangate-Authentication': token
       }
-    });
+    })
   }
 
   async call(options: AxiosRequestConfig): Promise<any> {
-    const instance = this.getInstance();
+    const instance = this.getInstance()
     try {
-      const resp = await instance(options);
-      return resp.data;
+      const resp = await instance(options)
+      return resp.data
     } catch (e) {
-      throw e;
+      throw e
     }
   }
 }
